aa375ea5d6e9ec1b21d531b44d29bd57aa202342
Adam Bibby <adam_bibby@hotmail.com>
2020-06-05 20:26:45 -0400

    IT WORKS

--- src/prepare.ts
+++ src/prepare.ts
@@ -18,35 +18,45 @@ export function prepare<
 type Union<T> = T extends Array<infer U> ? U : never
 
 type Filter<T, U> = T extends U ? T : never;  // Remove types from T that are not assignable to U
-type QueryNames<T extends GraphQLQuery<any, any>> = T['name']
+type QueryNames<T extends GraphQLQuery<any, any>> = {
+    [P in T['name']]: T extends GraphQLQuery<P, infer H> ? GraphQLQuery<P, H> : never
+}
 
 
 type GraphQLResult<Q extends GraphQLQuery<any, any>> = {
-    [P in Q['name']]: {
-        [Z in Filter<Union<Q['selects']>, string>]: Z extends string ? number : never
-    } & {
-        [Z in QueryNames<Filter<Union<Q['selects']>, GraphQLQuery<any, any>>>]: Z extends string ? number : never
-    }
+    [P in Q['name']]: Extra2<Q['selects']>
+}
+
+type Extra2<T> =
+    & { [Z in Filter<Union<T>, string>]: number }
+    & Extra<QueryNames<Filter<Union<T>, GraphQLQuery<any, any>>>>
+
+type Extra<T> = {
+    [P in keyof T]: T[P] extends GraphQLQuery<any, infer J> ? Extra2<J> : never
 }
 
 function run<Q extends GraphQLQuery<any, any>>(query: Q): GraphQLResult<Q> {
     return {} as any
 }
 
-const t1 = prepare('movie', {},
-    'title',
-    prepare('characters', {},
-        'name',
-        prepare('friends', {},
+const t1 = prepare('movie' as const, {},
+    'title' as const,
+    prepare('characters' as const, {},
+        'name' as const,
+        prepare('friends' as const, {},
             'relation'
         )
     ),
-    prepare('reviews', {},
-        'rating',
-        'website',
+    prepare('reviews' as const, {},
+        'rating' as const,
+        'website' as const,
+        prepare('friends' as const, {},
+            'relation' as const
+        )
     )
 )
 
 const t2 = run(t1)
-t2.movie.reviews
-t2.not_movie
+t2.movie.reviews.friends.relation
+t2.movie.characters
+t2.movie.title
